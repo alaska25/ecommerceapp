0ec119f41f668e010c4d8d7dc39b4875a4e8ece5
Arman <villegasarman23@gmail.com>
2022-08-07 23:55:46 -0700

    Added new code

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -71,7 +71,7 @@ const Cart = () =>{
 		<section className="py-4 container bg-light">
 			<div className="row justify-content-center">
 				<div className="col-12">
-					<h5 className="fw-bold">Cart ({totalUniqueItems}) Total Items: ({totalItems})</h5>
+					<h5 className="fw-bold">Cart ({totalUniqueItems}) Total Items: ({totalItems})Cart is Empty</h5>
 					<table className="table table-light table-hover m-0">
 					<tbody>
 						{items.map((item, index)=>{
@@ -85,7 +85,7 @@ const Cart = () =>{
 
 											<button className="btn btn-primary text-danger ms-2 mt-2" onClick={()=> updateItemQuantity(item.id, item.quantity - 1)}>-</button>
 											<button className="btn btn-primary text-warning ms-2 mt-2" onClick={()=> updateItemQuantity(item.id, item.quantity + 1)}>+</button>
-											<button size="sm" className="btn btn-danger text-primary ms-2 fw-bold" onClick={()=>removeItem(item.id)}>Remove Item</button>
+											<button size="sm" className="btn btn-danger text-light ms-2 fw-bold" onClick={()=>removeItem(item.id)}>Remove Item</button>
 											</td>
 										</tr>
 									)
